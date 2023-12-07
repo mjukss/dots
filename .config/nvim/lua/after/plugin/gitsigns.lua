5f763fbb95d9ab66b32033e03661c6360540e81d
mjukss <m.jukss@gmail.com>
2023-12-07 05:34:06 +0200

    Small refactor

--- .config/nvim/lua/after/plugin/gitsigns.lua
+++ .config/nvim/lua/after/plugin/gitsigns.lua
@@ -1,8 +1,6 @@
 require('gitsigns').setup {
   on_attach = function(bufnr)
     local gs = package.loaded.gitsigns
-
     vim.keymap.set('n', '<leader>gd', gs.diffthis)
-    vim.keymap.set('n', '<leader>gp', gs.preview_hunk)
   end
 }
--- .config/nvim/lua/setup/lazy.lua
+++ .config/nvim/lua/setup/lazy.lua
@@ -2,27 +2,26 @@ return require("lazy").setup({
   "stevearc/oil.nvim",
   "lewis6991/gitsigns.nvim",
   "nvim-treesitter/playground",
+
   { "tpope/vim-fugitive",              lazy = false },
   { "theprimeagen/harpoon",            branch = "harpoon2",                       dependencies = { "nvim-lua/plenary.nvim" } },
-
-  { 'rose-pine/neovim',                name = 'rose-pine' },
+  { "rose-pine/neovim",                name = "rose-pine" },
   { "nvim-telescope/telescope.nvim",   tag = "0.1.4",                             dependencies = { "nvim-lua/plenary.nvim" } },
   { "scalameta/nvim-metals",           dependencies = { "nvim-lua/plenary.nvim" } },
 
   { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
 
-
   {
     "VonHeikemen/lsp-zero.nvim",
     branch = "v3.x",
     dependencies = {
-      { 'neovim/nvim-lspconfig' }, -- Required
-      { "neovim/nvim-lspconfig" }, -- Required
-      'williamboman/mason.nvim',
-      'williamboman/mason-lspconfig.nvim',
-      { "hrsh7th/nvim-cmp" },     -- Required
-      { "hrsh7th/cmp-nvim-lsp" }, -- Required
-      { "L3MON4D3/LuaSnip" }      -- Required
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+      "neovim/nvim-lspconfig",
+      "neovim/nvim-lspconfig",
+      "hrsh7th/nvim-cmp",
+      "hrsh7th/cmp-nvim-lsp",
+      "L3MON4D3/LuaSnip",
     }
   }
 })
