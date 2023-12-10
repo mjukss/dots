b19e1512c882685f8943cbb414957611b9bfa326
mjukss <m.jukss@gmail.com>
2023-12-10 17:16:58 +0200

    Improve cmp

--- .config/nvim/lua/after/plugin/cmp.lua
+++ .config/nvim/lua/after/plugin/cmp.lua
@@ -1,9 +1,13 @@
 local cmp = require('cmp')
 
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
 cmp.setup({
   mapping = {
     ['<CR>'] = cmp.mapping.confirm({ select = true }),
     ['<Tab>'] = cmp.mapping.confirm({ select = true }),
     ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
   }
 })
